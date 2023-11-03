82284ded9cb89292a672f36e7914ac9031d037b2
dhanya081 <119044771+dhanya081@users.noreply.github.com>
2023-11-03 21:18:45 +0530

    DONE

---
+++ src/java/Gpay.java
@@ -0,0 +1,19 @@
+package java;
+
+public class Gpay {
+	private String name;
+	private String PhoneNumber;
+	private String email;
+	public void user()
+	{
+		name="Dhanya";
+		PhoneNumber="8951668692";
+		email="mpdhanya08@gmail.com";
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
